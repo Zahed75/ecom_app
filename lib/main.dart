757dbf9055984c6f01295f550d2c7062aa5cc5a1
Zahed75 <zahedhasan.cs@gmail.com>
2025-07-09 01:34:03 +0600

    Pubspec.yml updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
+import 'package:ecom_app/my_app.dart';
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp();
-  }
+void main() {
+  runApp(const MyApp());
 }
