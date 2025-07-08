d0739dd9d359040e966220b64a2a693bd265e610
Zahed75 <zahedhasan.cs@gmail.com>
2025-07-09 03:04:29 +0600

    ndk version updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:ecom_app/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  final WidgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: WidgetsBinding);
   runApp(const MyApp());
 }
